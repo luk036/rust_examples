f0ff76a64f250ce1fbe44bc88c7b404735b89e60
Wai-Shing Luk <luk036@gmail.com>
2023-01-22 10:14:05 +0800

    fmt can print chinese characters

--- examples/hello.cpp
+++ examples/hello.cpp
@@ -6,6 +6,6 @@
 #include <fmt/ranges.h>
 
 int main() {
-    auto arr = std::array{2, 3, 5}; // c++17
-    fmt::print("The ans is {}.\n", arr);
+  auto arr = std::array{2, 3, 5}; // c++17
+  fmt::print("答案是 {}.\n", arr);
 }
